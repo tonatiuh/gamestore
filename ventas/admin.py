54a2aa1cb52bb3aca531e7e44bc90384879b15a2
Tonatiuh Núñez <tonatiuhn@hotmail.es>
2012-01-24 06:03:30 -0600

    -ventas displays a list of bought  products

---
+++ ventas/admin.py
@@ -0,0 +1,4 @@
+from ventas.models import Venta_description
+from django.contrib import admin
+
+admin.site.register(Venta_description)
--- ventas/views.py
+++ ventas/views.py
@@ -1,11 +1,13 @@
-from ventas.models import Venta, VentaForm
+from ventas.models import Venta, VentaForm, Venta_description
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 
 def detail(request, id = None):
 	instance = None
+	latest_list = False
 	if id is not None:
 		instance = Venta.objects.get(id = id)
+		latest_list = Venta_description.objects.filter(venta = id)
 	#when POST
 	if request.method == 'POST':
 		form = VentaForm(request.POST, instance = instance)
@@ -15,4 +17,4 @@ def detail(request, id = None):
 	#when NOT POST
 	else:
 		form = VentaForm(instance = instance)
-	return render_to_response('store/ventas/detail.html',{'form':form})
+	return render_to_response('store/ventas/detail.html',{'form':form, 'latest_list':latest_list})
