3190dfeb91a28fadf6327f276e774d991d11950d
Tonatiuh Núñez <tonatiuhn@hotmail.es>
2012-01-24 22:29:03 -0600

    -ventas(CRU) with -details(CRU)

--- ventas/urls.py
+++ ventas/urls.py
@@ -8,8 +8,9 @@ urlpatterns = patterns('',
         ListView.as_view(
             queryset = Venta.objects.order_by('id'),
             context_object_name = 'latest_list', 
-            template_name = 'store/ventas/index.html')), #/
+            template_name = 'store/ventas/index.html')), #read
     url(r'^update/(?P<id_venta>\d+)/$', 'ventas.views.update'), #update
 
-	url(r'^update/(?P<id_venta>\d+)/details/create$', 'ventas.views.details_create'), #update/1/producto/
+	url(r'^update/(?P<id_venta>\d+)/details/create$', 'ventas.views.details_create'), #create
+	url(r'^update/(?P<id_venta>\d+)/details/update/(?P<id_ventadetail>\d+)$', 'ventas.views.details_update'), #update
 )
--- ventas/views.py
+++ ventas/views.py
@@ -15,20 +15,20 @@ def create(request):
 	return render_to_response('store/ventas/detail.html', {'form':form}, context_instance=RequestContext(request))
 
 def update(request, id_venta = None):
-	instance = None
+	venta = None
 	latest_list = False
 	if id is not None:
-		instance = Venta.objects.get(id = id_venta)
+		venta = Venta.objects.get(id = id_venta)
 		latest_list = VentaDetail.objects.filter(venta = id_venta)
 	#when POST
 	if request.method == 'POST':
-		form = VentaForm(request.POST, instance = instance)
+		form = VentaForm(request.POST, instance = venta)
 		if form.is_valid():
 			form.save()
 		return HttpResponseRedirect('/ventas/')
 	#when NOT POST
 	else:
-		form = VentaForm(instance = instance)
+		form = VentaForm(instance = venta)
 	return render_to_response('store/ventas/detail.html',{'form':form, 'latest_list':latest_list}, context_instance=RequestContext(request))
 
 def details_create(request, id_venta):
@@ -43,4 +43,20 @@ def details_create(request, id_venta):
 	#when NOT POST
 	else:
 		form = VentaDetailForm()
+	return render_to_response('store/common/detail.html', {'form':form}, context_instance=RequestContext(request))
+
+def details_update(request, id_venta, id_ventadetail):
+	#when POST
+	if request.method == 'POST':
+		venta = Venta.objects.get(id = id_venta)
+		ventadetail = VentaDetail.objects.get(id = id_ventadetail)
+		ventadetail.venta = venta
+		form = VentaDetailForm(request.POST, instance = ventadetail)
+		if form.is_valid():
+			form.save()
+		return HttpResponseRedirect('/ventas/update/'+id_venta)
+	#when NOT POST
+	else:
+		ventadetail = VentaDetail.objects.get(id = id_ventadetail)
+		form = VentaDetailForm(instance = ventadetail)
 	return render_to_response('store/common/detail.html', {'form':form}, context_instance=RequestContext(request))
